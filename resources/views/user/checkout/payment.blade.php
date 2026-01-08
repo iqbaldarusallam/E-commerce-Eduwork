97c575b13e85e99e5cb51095e1e12e0329307186
iqbaldarusallam <iqbaldarusallam@gmail.com>
2026-01-08 11:42:52 +0700

    Update checkout payment view

--- resources/views/user/checkout/payment.blade.php
+++ resources/views/user/checkout/payment.blade.php
@@ -185,7 +185,7 @@
                             await notifyServer(result.order_id);
                             hideLoading();
                             if (NF && NF.Notify) NF.Notify.success('Payment successful.');
-                            window.location.href = '/user/dashboard?payment=success&order=' + result.order_id;
+                            window.location.href = '{{ route('user.home') }}?payment=success&order=' + result.order_id;
                         },
                         onPending: function(result) {
                             hideLoading();
